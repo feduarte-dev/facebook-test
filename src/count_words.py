052d8fb238905e7909a7ef0214daddaa5193d9a4
Felipe Duarte <feduarte.dev@gmail.com>
2024-03-12 11:37:50 -0300

    exercicio pronto

--- src/count_words.py
+++ src/count_words.py
@@ -1,2 +1,20 @@
 def count_words(words: list[str], chars: str) -> int:
-    raise NotImplementedError
+    result = 0
+    chars_count = {char: chars.count(char) for char in set(chars)}
+
+    for word in words:
+        word_count = {char: word.count(char) for char in set(word)}
+        if all(
+            word_count[char] <= chars_count.get(char, 0) for char in word_count
+        ):
+            result += len(word)
+
+    return result
+
+
+# words = ["cat", "bt", "hat", "tree"], chars = "atach"
+
+# words = ["hello", "world", "students"], chars = "welldonehoneyr"
+
+
+print(count_words(["cat", "bt", "hat", "tree"], "atach"))
